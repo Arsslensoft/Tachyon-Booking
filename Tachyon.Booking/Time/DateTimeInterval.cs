59caa49cb35fc7cf91e7311af642b6d243d9e84d
Arsslen Idadi <arsslens021@gmail.com>
2020-09-20 19:17:30 +0100

    compare/equaltable implementation

--- Tachyon.Booking/Time/DateTimeInterval.cs
+++ Tachyon.Booking/Time/DateTimeInterval.cs
@@ -2,9 +2,26 @@
 
 namespace Tachyon.Booking.Time
 {
-    public struct DateTimeInterval
+    public struct DateTimeInterval : IEquatable<DateTimeInterval>, IComparable<DateTimeInterval>
     {
         public DateTime Start { get; set; }
         public DateTime Due { get; set; }
+
+        public DateTimeInterval(DateTime start, DateTime due)
+        {
+            Start = start;
+            Due = due;
+        }
+
+        public bool Equals(DateTimeInterval other)
+        {
+            return other.Start == Start && other.Due == Due;
+        }
+
+        public int CompareTo(DateTimeInterval other)
+        {
+            var startResult = Start.CompareTo(other.Start);
+            return startResult == 0 ? Due.CompareTo(other.Due) : startResult;
+        }
     }
 }
--- Tachyon.Booking/Time/TimeInterval.cs
+++ Tachyon.Booking/Time/TimeInterval.cs
@@ -2,9 +2,26 @@
 
 namespace Tachyon.Booking.Time
 {
-    public struct TimeInterval
+    public struct TimeInterval : IEquatable<TimeInterval>, IComparable<TimeInterval>
     {
         public TimeSpan Start { get; set; }
         public TimeSpan Due { get; set; }
+
+        public TimeInterval(TimeSpan start, TimeSpan due)
+        {
+            Start = start;
+            Due = due;
+        }
+
+        public bool Equals(TimeInterval other)
+        {
+            return other.Start == Start && other.Due == Due;
+        }
+
+        public int CompareTo(TimeInterval other)
+        {
+            var startResult = Start.CompareTo(other.Start);
+            return startResult == 0 ? Due.CompareTo(other.Due) : startResult;
+        }
     }
 }
